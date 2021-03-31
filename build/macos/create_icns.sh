c982f662385f9af14f93c2752def8e89b796b615
Niklas Eicker <git@nikl.me>
2021-03-31 21:53:24 +0200

    Add link to Applications to macOS bundle

--- build/macos/create_icns.sh
+++ build/macos/create_icns.sh
@@ -11,5 +11,5 @@ sips -z 512 512   icon_1024x1024.png --out AppIcon.iconset/icon_256x256@2x.png
 sips -z 512 512   icon_1024x1024.png --out AppIcon.iconset/icon_512x512.png
 cp icon_1024x1024.png AppIcon.iconset/icon_512x512@2x.png
 iconutil -c icns AppIcon.iconset
-mkdir -p Game.app/Contents/Resources
-mv AppIcon.icns Game.app/Contents/Resources/
+mkdir -p src/Game.app/Contents/Resources
+mv AppIcon.icns src/Game.app/Contents/Resources/
