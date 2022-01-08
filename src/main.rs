6d795efe8ac7c129119b45759a2cbd68390a7258
Niklas Eicker <git@nikl.me>
2022-01-08 21:24:12 +0100

    Update to Bevy version 0.6

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::{App, ClearColor, Color, WindowDescriptor};
+use bevy::prelude::{App, ClearColor, Color, WindowDescriptor, Msaa};
 use bevy::DefaultPlugins;
 use bevy_game::GamePlugin;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa { samples: 1 })
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .insert_resource(WindowDescriptor {
             width: 800.,
