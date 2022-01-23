8d7638a44fc48e752d2164acebc19a66969fd340
Matthew Bakhtiari <dev@mtbk.me>
2022-01-23 01:06:42 +0100

    remove `.clone()` from `button_colors.*`

--- src/menu.rs
+++ src/menu.rs
@@ -48,7 +48,7 @@ fn setup_menu(
                 align_items: AlignItems::Center,
                 ..Default::default()
             },
-            color: button_colors.normal.clone(),
+            color: button_colors.normal,
             ..Default::default()
         })
         .insert(PlayButton)
@@ -88,10 +88,10 @@ fn click_play_button(
                 state.set(GameState::Playing).unwrap();
             }
             Interaction::Hovered => {
-                *color = button_colors.hovered.clone();
+                *color = button_colors.hovered;
             }
             Interaction::None => {
-                *color = button_colors.normal.clone();
+                *color = button_colors.normal;
             }
         }
     }
