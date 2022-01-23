e06278cce1e3316afb8c2970264ad739f26c9844
Niklas Eicker <git@nikl.me>
2022-01-23 11:33:27 +0100

    Add CI workflow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::{App, ClearColor, Color, WindowDescriptor, Msaa};
+use bevy::prelude::{App, ClearColor, Color, Msaa, WindowDescriptor};
 use bevy::DefaultPlugins;
 use bevy_game::GamePlugin;
 
