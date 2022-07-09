2ddbef62a5b019fd329f840926f887420f49fb00
Niklas Eicker <git@nikl.me>
2022-07-09 10:18:16 +0200

    Try to set window icon on Windows and X11

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::{App, ClearColor, Color, Msaa, WindowDescriptor};
+use bevy::prelude::{App, ClearColor, Color, Msaa, NonSend, WindowDescriptor};
+use bevy::window::WindowId;
+use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
 use bevy_game::GamePlugin;
+use winit::window::Icon;
 
 fn main() {
     App::new()
@@ -17,5 +20,17 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(GamePlugin)
+        .add_startup_system(set_window_icon)
         .run();
+}
+
+// Sets the icon on windows and X11
+fn set_window_icon(windows: NonSend<WinitWindows>) {
+    let primary = windows.get_window(WindowId::primary()).unwrap();
+    if let Ok(image) = image::open("assets/textures/bevy.png").map(|image| image.into_rgba8()) {
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
 }
