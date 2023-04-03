68d4814c73327d17b2e92c5bf14f85886187b99b
chickenchicken9 <127390746+chickenchicken9@users.noreply.github.com>
2023-04-03 12:13:00 +0000

    Fix comment spacing.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
-use bevy_game::GamePlugin;  // ToDo: Replace bevy_game with your new crate name.
+use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
 use std::io::Cursor;
 use winit::window::Icon;
 
