d7d83fe5ee84163d72d3b314ba7790129258ef2c
Niklas Eicker <git@nikl.me>
2023-07-08 18:17:17 +0200

    Broken touch controls

--- src/loading.rs
+++ src/loading.rs
@@ -36,6 +36,6 @@ pub struct AudioAssets {
 
 #[derive(AssetCollection, Resource)]
 pub struct TextureAssets {
-    #[asset(path = "textures/bevy.png")]
+    #[asset(path = "res/mipmap-mdpi/bevy.png")]
     pub texture_bevy: Handle<Image>,
 }
