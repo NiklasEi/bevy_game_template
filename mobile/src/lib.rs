222ff8bc5c63de38fed2dc580903176196a477fd
Niklas Eicker <git@nikl.me>
2023-07-15 19:20:54 +0200

    Update mobile lib to Bevy 0.11

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -3,8 +3,5 @@ use bevy_game::GamePlugin;
 
 #[bevy_main]
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(GamePlugin)
-        .run()
+    App::new().add_plugins((DefaultPlugins, GamePlugin)).run()
 }
