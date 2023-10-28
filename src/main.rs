783d66e779dc9f42f37864f63f3e0a8f6f34e32d
Niklas Eicker <git@nikl.me>
2023-10-28 13:59:54 +0200

    Change window size to default and add loading indicator for wasm file

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,10 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Bevy game".to_string(), // ToDo
-                resolution: (800., 600.).into(),
                 // Bind to canvas included in `index.html`
                 canvas: Some("#bevy".to_owned()),
+                // The canvas size is constrained in index.html and build/web/styles.css
+                fit_canvas_to_parent: true,
                 // Tells wasm not to override default event handling, like F5 and Ctrl+R
                 prevent_default_event_handling: false,
                 ..default()
