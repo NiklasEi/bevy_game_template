6f89321a257fee65ed73c245c3eb3ac911447606
Niklas Eicker <git@nikl.me>
2025-01-03 18:09:32 +0100

    Fix the android setup by enabling the android-native-activity feature

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
 use bevy::window::WindowMode;
+use bevy::winit::WinitSettings;
 use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
 
 #[bevy_main]
 fn main() {
     App::new()
+        .insert_resource(WinitSettings::mobile())
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
