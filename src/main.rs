c252481471234440be62c80d579f062cca3624cf
Niklas Eicker <git@nikl.me>
2025-10-26 15:38:11 +0100

    Update to Bevy 0.17

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::asset::AssetMetaCheck;
+use bevy::ecs::system::NonSendMarker;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
-use bevy::winit::WinitWindows;
+use bevy::winit::WINIT_WINDOWS;
 use bevy::DefaultPlugins;
 use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
 use std::io::Cursor;
@@ -39,23 +40,24 @@ fn main() {
 
 // Sets the icon on windows and X11
 fn set_window_icon(
-    windows: NonSend<WinitWindows>,
-    primary_window: Query<Entity, With<PrimaryWindow>>,
+    primary_window: Single<Entity, With<PrimaryWindow>>,
+    _non_send_marker: NonSendMarker,
 ) -> Result {
-    let primary_entity = primary_window.single()?;
-    let Some(primary) = windows.get_window(primary_entity) else {
-        return Err(BevyError::from("No primary window!"));
-    };
-    let icon_buf = Cursor::new(include_bytes!(
-        "../build/macos/AppIcon.iconset/icon_256x256.png"
-    ));
-    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
-        let image = image.into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        let icon = Icon::from_rgba(rgba, width, height).unwrap();
-        primary.set_window_icon(Some(icon));
-    };
+    WINIT_WINDOWS.with_borrow(|windows| {
+        let Some(primary) = windows.get_window(*primary_window) else {
+            return Err(BevyError::from("No primary window!"));
+        };
+        let icon_buf = Cursor::new(include_bytes!(
+            "../build/macos/AppIcon.iconset/icon_256x256.png"
+        ));
+        if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+            let image = image.into_rgba8();
+            let (width, height) = image.dimensions();
+            let rgba = image.into_raw();
+            let icon = Icon::from_rgba(rgba, width, height).unwrap();
+            primary.set_window_icon(Some(icon));
+        };
 
-    Ok(())
+        Ok(())
+    })
 }
